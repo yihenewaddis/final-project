4ea93b861beca9355710ee0d6af8f876d01d67f2
yihenewaddis <addisyihenew@gmail.com>
2023-02-03 23:58:52 +0300

    first commit

---
+++ js/home.js
@@ -0,0 +1,18 @@
+                                               
+
+setTimeout(second,2000)
+setInterval(first,6000)
+function first(){
+  document.getElementById("animat").innerHTML="we provide best medical service"
+  setTimeout(second,2000)
+}
+
+function second(){
+  document.getElementById("animat").innerHTML="24 Hours Services & professional staff"
+  setTimeout(serd,2000)
+}
+
+function serd(){
+  document.getElementById("animat").innerHTML="Emergency Care & quality service"
+  setTimeout(2000)
+}
